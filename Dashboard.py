546fc94550266213f832d044e84f031d9a8a47e5
diniathy <160316871+diniathy@users.noreply.github.com>
2024-03-04 14:33:21 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
-sns.set(style='dark')
 
 day_path = "Bike-sharing-dataset\day.csv"
 day_df = pd.read_csv(day_path)
